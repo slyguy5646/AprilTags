d76b1ac0017b5577db69569d157a9f110d65538a
slyguy5646 <lpmonaghan06@yahoo.com>
2022-09-29 19:30:28 -0400

    added camera calibration script

---
+++ CameraCalibration/calibrate.py
@@ -0,0 +1,72 @@
+import numpy as np
+import cv2
+import glob
+
+checkerBoardSize = (7,7)
+frameSize = (1280, 720)
+
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+
+objp = np.zeros((checkerBoardSize[0] * checkerBoardSize[1], 3), np.float32)
+objp[:,:2] = np.mgrid[0:checkerBoardSize[0], 0:checkerBoardSize[1]].T.reshape(-1, 2)
+
+objPoints = []
+imgPoints = []
+
+images = glob.glob('*.png')
+
+for image in images:
+    print(image)
+    img = cv2.imread(image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    ret, corners = cv2.findChessboardCorners(gray, checkerBoardSize, None)
+
+
+    if ret == True:
+        objPoints.append(objp)
+        corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgPoints.append(corners)
+
+        cv2.drawChessboardCorners(img, checkerBoardSize, corners2, ret)
+        cv2.imshow('img', img)
+        cv2.waitKey(1000)
+
+cv2.destroyAllWindows()
+
+ret, cameraMatrix, dist, rvecs, tvecs = cv2.calibrateCamera(objPoints, imgPoints, frameSize, None, None)
+
+# print("camera calibrate: ", ret)
+print(cameraMatrix)
+# print(f'Distortion:\n{dist}')
+# print(f'RotationVectors:\n{rvecs}')
+# print(f'TranslationalVectors:\n{tvecs}')
+
+
+img = cv2.imread('opencv_frame_0.png')
+h, w = img.shape[:2]
+newCameraMatrix, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix, dist, (w,h), 1, (w,h))
+
+#undistort
+dst = cv2.undistort(img, cameraMatrix, dist, None, newCameraMatrix)
+#crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv2.imwrite('result.png', dst)
+
+
+print(newCameraMatrix)
+
+
+
+
+
+
+
+
+
+
+
+
+
