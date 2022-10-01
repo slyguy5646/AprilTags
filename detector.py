fixed distance calculation but it still is off when the cam is skewed

detector.py
@@ -47,7 +47,7 @@ def main():
     cv2.namedWindow(dataWindow, cv2.WINDOW_NORMAL)
     whiteBackground = np.full((1000, 1000, 1), 255, dtype = "uint8") #set window to white on startup
     ########
-    
+
     detector = apriltag.Detector(options,
                                  searchpath=apriltag._get_demo_searchpath())
 